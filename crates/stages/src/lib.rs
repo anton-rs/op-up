c89fd64a9a4e6af926d2474176876ee14a0f5b2d
steph-mipt <steph.sinyakov@gmail.com>
2023-10-04 18:38:51 +0300

    fix(stages): WIP `deploy` contracts to L1

--- crates/stages/src/lib.rs
+++ crates/stages/src/lib.rs
@@ -97,19 +97,38 @@ impl Stages {
         tracing::info!(target: "opup", "Building devnet...");
         self.config.create_artifacts_dir()?;
         let curr_timestamp = genesis::current_timestamp();
-        let genesis_template = genesis::genesis_template_string(curr_timestamp)
-            .ok_or_else(|| eyre::eyre!("Could not create genesis template"))?;
 
         // Step 1.
         // Create L1 genesis
         if !genesis_l1_file.exists() {
             tracing::info!(target: "opup", "Creating L1 genesis...");
+            let genesis_template = genesis::genesis_template_string(curr_timestamp)
+                .ok_or_else(|| eyre::eyre!("Could not create genesis template"))?;
             if let Some(parent_dir) = genesis_l1_file.parent() {
                 std::fs::create_dir_all(parent_dir)?;
             }
             std::fs::write(genesis_l1_file, genesis_template)?;
         } else {
             tracing::info!(target: "opup", "L1 genesis already found.");
+        }
+
+        if !genesis_l2_file.exists() {
+            tracing::info!(target: "opup", "Do some stuff to get the L2 genesis...");
+
+            let bin_dir = op_monorepo_dir.join("op-program/bin");
+            if !std::fs::metadata(bin_dir).is_ok() {
+                let make_command = Command::new("make")
+                    .args(["cannon-prestate"])
+                    .current_dir(&op_monorepo_dir)
+                    .output()?;
+                check_command(make_command, "Failed to do cannon prestate")?;
+            }
+            // TODO: check is allocs here actually do what we want
+            // let allocs = Command::new("make")
+            //     .args(["devnet-allocs"])
+            //     .current_dir(&op_monorepo_dir)
+            //     .output()?;
+            // check_command(allocs, "Failed to do allocs")?;
         }
 
         // Step 2.
@@ -126,15 +145,6 @@ impl Stages {
         check_command(start_l1, "Failed to start L1 execution client")?;
         net::wait_up(L1_PORT, 10, 1)?;
 
-        if !genesis_l2_file.exists() {
-            tracing::info!(target: "opup", "Creating L2 and rollup genesis...");
-            let l2_genesis = Command::new("make")
-                .args(["devnet-allocs"])
-                .current_dir(&op_monorepo_dir)
-                .output()?;
-            check_command(l2_genesis, "Failed to create L2 genesis")?;
-        }
-
         // Step 3.
         // Generate network configs
         tracing::info!(target: "opup", "Generating network configs...");
@@ -150,20 +160,58 @@ impl Stages {
         // Step 4.
         // Deploy contracts
         let addresses = if !addresses_json_file.exists() {
-            println!("Deploying contracts...");
-            let install_deps = Command::new("yarn")
+            tracing::info!(target: "opup", "Deploying contracts...");
+            let install_deps = Command::new("pnpm")
                 .args(["install"])
                 .current_dir(&contracts_bedrock_dir)
                 .output()?;
             check_command(install_deps, "Failed to install dependencies")?;
 
-            let deploy_contracts = Command::new("yarn")
-                .args(["hardhat", "--network", "devnetL1", "deploy", "--tags", "l1"])
-                .env("CHAIN_ID", "900")
-                .env("L1_RPC", L1_URL)
-                .env("PRIVATE_KEY_DEPLOYER", DEPLOYER_PRIVATE_KEY)
-                .current_dir(&contracts_bedrock_dir)
-                .output()?;
+            // let deploy_contracts = Command::new("yarn")
+            //     .args(["hardhat", "--network", "devnetL1", "deploy", "--tags", "l1"])
+            //     .env("CHAIN_ID", "900")
+            //     .env("L1_RPC", L1_URL)
+            //     .env("PRIVATE_KEY_DEPLOYER", DEPLOYER_PRIVATE_KEY)
+            //     .current_dir(&contracts_bedrock_dir)
+            //     .output()?;
+            let deploy_contracts = {
+                let send_eth = Command::new("cast")
+                    .args(["send"])
+                    .args(["--from", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"])
+                    .args(["--private-key", "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"])
+                    .args(["--rpc-url", L1_URL])
+                    .args(["--unlocked", "--value", "1ether", "0x3fAB184622Dc19b6109349B94811493BF2a45362"])
+                    .current_dir(&contracts_bedrock_dir)
+                    .output()?;
+                check_command(send_eth, "Failed to send eth to create2")?;
+
+                let deploy_1 = Command::new("cast")
+                    .args(["publish"])
+                    .args(["--rpc-url", L1_URL])
+                    .args(["--private-key", "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"])
+                    .args(["0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222"])
+                    .current_dir(&contracts_bedrock_dir)
+                    .output()?;
+                check_command(deploy_1, "Failed to deploy the create2 deployer")?;
+
+                let deploy_2 = Command::new("forge")
+                    .args(["script", "scripts/Deploy.s.sol:Deploy"])
+                    .args(["--sender", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"])
+                    .args(["--private-key", "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"])
+                    .args(["--rpc-url", L1_URL])
+                    .args(["--broadcast", "--unlocked"])
+                    .current_dir(&contracts_bedrock_dir)
+                    .output()?;
+                check_command(deploy_2, "Failed to deploy the create2 deployer (forge)")?;
+
+                tracing::info!(target: "opup", "Syncing contracts.");
+                Command::new("forge")
+                    .args(["script", "scripts/Deploy.s.sol:Deploy"])
+                    .args(["--sig", "sync()"])
+                    .args(["--rpc-url", L1_URL])
+                    .current_dir(&contracts_bedrock_dir)
+                    .output()?
+            };
 
             check_command(deploy_contracts, "Failed to deploy contracts")?;
 
