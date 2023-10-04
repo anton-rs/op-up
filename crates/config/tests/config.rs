cfb9a3f89983f6427fab008eae881867aeaf80a6
refcell <refcell@oplabs.co>
2023-10-04 00:39:57 +0000

    Fix tempdir tests

--- crates/config/tests/config.rs
+++ crates/config/tests/config.rs
@@ -1,6 +1,7 @@
 use op_config::Config;
 use op_primitives::{ChallengerAgent, L1Client, L2Client, RollupClient};
 use std::path::PathBuf;
+use temp_testdir::TempDir;
 
 #[test]
 fn test_default_config() {
@@ -19,12 +20,9 @@ fn test_default_config() {
 
 #[test]
 fn test_read_config_from_toml() {
-    // Create a temporary directory and set it as the current working directory.
-    // This directory will be deleted when the `tmpdir` variable goes out of scope.
-    let tmpdir = tempfile::tempdir().unwrap();
-    std::env::set_current_dir(&tmpdir).unwrap();
+    let tempdir = TempDir::default().permanent();
+    std::env::set_current_dir(&tempdir).unwrap();
 
-    // Write a toml config to the temporary directory.
     std::fs::write(
         "stack.toml",
         r#"
@@ -40,7 +38,6 @@ fn test_read_config_from_toml() {
     .unwrap();
     assert!(PathBuf::from("stack.toml").exists());
 
-    // Create a config from the toml file.
     let config = Config::from_toml("stack.toml").unwrap();
     assert_eq!(config.artifacts, PathBuf::from(Config::STACK_DIR_NAME));
     assert_eq!(config.l1_client, L1Client::Reth);
@@ -49,27 +46,15 @@ fn test_read_config_from_toml() {
     assert_eq!(config.challenger, ChallengerAgent::OpChallengerGo);
     assert_eq!(config.enable_sequencing, true);
     assert_eq!(config.enable_fault_proofs, true);
-
-    // Drop the `tmpdir` variable, which deletes the temporary directory.
-    drop(tmpdir);
 }
 
 #[test]
 fn test_create_artifacts_dir() {
-    // Create a temporary directory and set it as the current working directory.
-    // This directory will be deleted when the `tmpdir` variable goes out of scope.
-    let tmpdir = tempfile::tempdir().unwrap();
-    std::env::set_current_dir(&tmpdir).unwrap();
+    let tempdir = TempDir::default().permanent();
+    std::env::set_current_dir(&tempdir).unwrap();
 
-    // Create a default configuration and create the artifact directory.
     let config = Config::default();
     config.create_artifacts_dir().unwrap();
     assert!(config.artifacts.exists());
     assert!(config.artifacts.is_dir());
-
-    // Drop the `tmpdir` variable, which deletes the temporary directory.
-    drop(tmpdir);
-
-    // The temporary directory should no longer exist.
-    assert!(!config.artifacts.exists());
 }
