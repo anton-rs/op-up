e179ed8e7fb1d7e4a963cbfefb5afd8cc06601b0
steph-mipt <steph.sinyakov@gmail.com>
2023-10-06 00:17:19 +0300

    fix(stages): module updated and the `op-up` work restored

--- crates/stages/src/lib.rs
+++ crates/stages/src/lib.rs
@@ -12,7 +12,6 @@ use std::path::Path;
 use std::process::Command;
 
 use op_config::Config;
-use op_contracts::AddressManager;
 use op_primitives::genesis;
 
 mod commands;
@@ -38,10 +37,10 @@ pub const L2_URL: &str = "http://localhost:9545";
 pub const L2_PORT: u16 = 9545;
 
 /// Rollup node url.
-pub const ROLLUP_URL: &str = "http://localhost:7545";
+pub const OP_NODE_URL: &str = "http://localhost:7545";
 
 /// Rollup node port.
-pub const ROLLUP_PORT: u16 = 7545;
+pub const OP_NODE_PORT: u16 = 7545;
 
 /// Testing deployer private key.
 pub const DEPLOYER_PRIVATE_KEY: &str =
@@ -79,7 +78,6 @@ impl Stages {
         let genesis_rollup_file = devnet_dir.join("rollup.json");
         let allocs_file = devnet_dir.join("allocs-l1.json");
         let addresses_json_file = devnet_dir.join("addresses.json");
-        let addresses_sdk_json_file = devnet_dir.join("addresses-sdk.json");
         let deploy_config_file = deploy_config_dir.join("devnetL1.json");
 
         // Check if the optimism and optimism-rs paths exist in the project root dir.
@@ -98,26 +96,28 @@ impl Stages {
         tracing::info!(target: "opup", "Building devnet...");
         self.config.create_artifacts_dir()?;
         let curr_timestamp = genesis::current_timestamp();
-
-        // Step 1.
-        // Create L1 genesis template
-        if !genesis_l1_file.exists() {
-            tracing::info!(target: "opup", "Creating L1 genesis...");
-            let genesis_template = genesis::genesis_template_string(curr_timestamp)
-                .ok_or_else(|| eyre::eyre!("Could not create genesis template"))?;
-            if let Some(parent_dir) = genesis_l1_file.parent() {
-                std::fs::create_dir_all(parent_dir)?;
-            }
-            std::fs::write(genesis_l1_file.clone(), genesis_template)?;
-        } else {
-            tracing::info!(target: "opup", "L1 genesis already found.");
+        if !devnet_dir.exists() {
+            std::fs::create_dir_all(&devnet_dir)?;
         }
 
-        if !genesis_l2_file.exists() {
-            tracing::info!(target: "opup", "Do some stuff to get the L2 genesis...");
+        // Step 1.
+        // Generate deploy config
 
+        tracing::info!(target: "opup", "Generating deploy config...");
+        let mut deploy_config = json::read_json(&deploy_config_file)?;
+        let hex_timestamp = format!("{:#x}", curr_timestamp);
+        json::set_json_property(&mut deploy_config, "l1GenesisBlockTimestamp", hex_timestamp);
+        json::set_json_property(&mut deploy_config, "l1StartingBlockTag", "earliest");
+        json::write_json(&deploy_config_file, &deploy_config)?;
+
+        // Step 2.
+        // Create prestate and allocs
+
+        // TODO: is this condition correct?
+        if !genesis_l2_file.exists() {
+            tracing::info!(target: "opup", "Making prestate and allocs...");
             let bin_dir = op_monorepo_dir.join("op-program/bin");
-            if !std::fs::metadata(bin_dir).is_ok() {
+            if std::fs::metadata(bin_dir).is_err() {
                 let make_command = Command::new("make")
                     .args(["cannon-prestate"])
                     .current_dir(&op_monorepo_dir)
@@ -130,42 +130,40 @@ impl Stages {
                 .current_dir(&op_monorepo_dir)
                 .output()?;
             check_command(allocs, "Failed to do allocs")?;
-
-            let _ = Command::new("cp")
+            let copy_addr = Command::new("cp")
                 .args([".devnet/addresses.json", "../.devnet/"])
                 .current_dir(&op_monorepo_dir)
                 .output()?;
-            let copy = Command::new("cp")
+            check_command(copy_addr, "Failed to do copy of addresses.json")?;
+            let copy_allocs = Command::new("cp")
                 .args([".devnet/allocs-l1.json", "../.devnet/"])
                 .current_dir(&op_monorepo_dir)
                 .output()?;
-            check_command(copy, "Failed to do copy of addresses.json and allocs-l1.json")?;
+            check_command(copy_allocs, "Failed to do copy of allocs.json")?;
         }
 
         // Step 3.
-        // Generate network configs
-        tracing::info!(target: "opup", "Generating network configs...");
-        let mut deploy_config = json::read_json(&deploy_config_file)?;
-        let hex_timestamp = format!("{:#x}", curr_timestamp);
-        json::set_json_property(
-            &mut deploy_config,
-            "l1GenesisBlockTimestamp",
-            hex_timestamp,
-        );
-        json::set_json_property(&mut deploy_config, "l1StartingBlockTag", "earliest");
-        json::write_json(&deploy_config_file, &deploy_config)?;
+        // Create L1 genesis
 
-        let l1_genesis = Command::new("go")
-            .args(["run", "cmd/main.go", "genesis", "l1"])
-            .args(["--deploy-config", deploy_config_file.to_str().unwrap()])
-            .args(["--l1-allocs", allocs_file.to_str().unwrap()])
-            .args(["--l1-deployments", addresses_json_file.to_str().unwrap()])
-            .args(["--outfile.l1", genesis_l1_file.to_str().unwrap()])
-            .current_dir(&op_node_dir)
-            .output()?;
-        check_command(l1_genesis, "Failed to create L1 genesis")?;
+        if !genesis_l1_file.exists() {
+            tracing::info!(target: "opup", "Creating L1 genesis...");
+            let genesis_template = genesis::genesis_template_string(curr_timestamp)
+                .ok_or_else(|| eyre::eyre!("Could not create genesis template"))?;
+            std::fs::write(genesis_l1_file.clone(), genesis_template)?;
+            let l1_genesis = Command::new("go")
+                .args(["run", "cmd/main.go", "genesis", "l1"])
+                .args(["--deploy-config", deploy_config_file.to_str().unwrap()])
+                .args(["--l1-allocs", allocs_file.to_str().unwrap()])
+                .args(["--l1-deployments", addresses_json_file.to_str().unwrap()])
+                .args(["--outfile.l1", genesis_l1_file.to_str().unwrap()])
+                .current_dir(&op_node_dir)
+                .output()?;
+            check_command(l1_genesis, "Failed to create L1 genesis")?;
+        } else {
+            tracing::info!(target: "opup", "L1 genesis already found.");
+        }
 
-        // Step 2.
+        // Step 4.
         // Start L1 execution client
 
         tracing::info!(target: "opup", "Starting L1 execution client...");
@@ -179,73 +177,12 @@ impl Stages {
         check_command(start_l1, "Failed to start L1 execution client")?;
         net::wait_up(L1_PORT, 10, 1)?;
 
-        // Step 4.
-        // Deploy contracts
-        let addresses = if !addresses_json_file.exists() {
-            tracing::info!(target: "opup", "Deploying contracts...");
-            let install_deps = Command::new("pnpm")
-                .args(["install"])
-                .current_dir(&contracts_bedrock_dir)
-                .output()?;
-            check_command(install_deps, "Failed to install dependencies")?;
-
-            // let deploy_contracts = Command::new("yarn")
-            //     .args(["hardhat", "--network", "devnetL1", "deploy", "--tags", "l1"])
-            //     .env("CHAIN_ID", "900")
-            //     .env("L1_RPC", L1_URL)
-            //     .env("PRIVATE_KEY_DEPLOYER", DEPLOYER_PRIVATE_KEY)
-            //     .current_dir(&contracts_bedrock_dir)
-            //     .output()?;
-            let deploy_contracts = {
-                let send_eth = Command::new("cast")
-                    .args(["send"])
-                    .args(["--from", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"])
-                    .args(["--rpc-url", L1_URL])
-                    .args(["--unlocked", "--value", "1ether", "0x3fAB184622Dc19b6109349B94811493BF2a45362"])
-                    .current_dir(&contracts_bedrock_dir)
-                    .output()?;
-                check_command(send_eth, "Failed to send eth to create2")?;
-
-                let deploy_1 = Command::new("cast")
-                    .args(["publish"])
-                    .args(["--rpc-url", L1_URL])
-                    .args(["0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222"])
-                    .current_dir(&contracts_bedrock_dir)
-                    .output()?;
-                check_command(deploy_1, "Failed to deploy the create2 deployer")?;
-
-                let deploy_2 = Command::new("forge")
-                    .args(["script", "scripts/Deploy.s.sol:Deploy"])
-                    .args(["--sender", "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"])
-                    .args(["--rpc-url", L1_URL])
-                    .args(["--broadcast", "--unlocked"])
-                    .current_dir(&contracts_bedrock_dir)
-                    .output()?;
-                check_command(deploy_2, "Failed to deploy the create2 deployer (forge)")?;
-
-                tracing::info!(target: "opup", "Syncing contracts.");
-                Command::new("forge")
-                    .args(["script", "scripts/Deploy.s.sol:Deploy"])
-                    .args(["--sig", "sync()"])
-                    .args(["--rpc-url", L1_URL])
-                    .current_dir(&contracts_bedrock_dir)
-                    .output()?
-            };
-
-            check_command(deploy_contracts, "Failed to deploy contracts")?;
-
-            // Write the addresses to json
-            let (addresses, sdk_addresses) = AddressManager::set_addresses(&deployment_dir)?;
-            json::write_json(&addresses_json_file, &addresses)?;
-            json::write_json(&addresses_sdk_json_file, &sdk_addresses)?;
-
-            addresses
-        } else {
-            tracing::info!(target: "opup", "Contracts already deployed.");
-            json::read_json(&addresses_json_file)?
-        };
-
         // Step 5.
+        // Bind the addresses (contracts already deployed by "allocs" step)
+
+        let addresses = json::read_json(&addresses_json_file)?;
+
+        // Step 6.
         // Create L2 genesis
 
         if !genesis_l2_file.exists() {
@@ -264,12 +201,12 @@ impl Stages {
             tracing::info!(target: "opup", "L2 genesis already found.");
         }
 
-        // Step 6.
+        // Step 7.
         // Start L2 execution client
 
-        println!("Starting L2 execution client...");
+        tracing::info!(target: "opup", "Starting L2 execution client...");
         let start_l2 = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "l2"])
+            .args(["up", "-d", "l2"])
             .env("PWD", docker_dir.to_str().unwrap())
             .env("L2_CLIENT_CHOICE", self.config.l2_client.to_string())
             .current_dir(&docker_dir)
@@ -277,41 +214,13 @@ impl Stages {
         check_command(start_l2, "Failed to start L2 execution client")?;
         net::wait_up(L2_PORT, 10, 1)?;
 
-        // Step 7.
-        // Start rollup client
-
-        println!("Starting rollup client...");
-        let start_rollup = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "rollup-client"])
-            .env("PWD", docker_dir.to_str().unwrap())
-            .env(
-                "ROLLUP_CLIENT_CHOICE",
-                self.config.rollup_client.to_string(),
-            )
-            .current_dir(&docker_dir)
-            .output()?;
-        check_command(start_rollup, "Failed to start rollup client")?;
-        net::wait_up(ROLLUP_PORT, 30, 1)?;
-
         // Step 8.
-        // Start proposer
+        // Start other services
 
-        println!("Starting proposer...");
-        let start_proposer = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "proposer"])
-            .env("PWD", docker_dir.to_str().unwrap())
-            .env("L2OO_ADDRESS", addresses["L2OutputOracleProxy"].to_string())
-            .current_dir(&docker_dir)
-            .output()?;
-        check_command(start_proposer, "Failed to start proposer")?;
-
-        // Step 9.
-        // Start batcher
-
-        println!("Starting batcher...");
+        tracing::info!(target: "opup", "Bringing up everything else...");
         let rollup_config = json::read_json(&genesis_rollup_file)?;
-        let start_batcher = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "batcher"])
+        let start_rollup = Command::new("docker-compose")
+            .args(["up", "-d", "node", "proposer", "batcher"])
             .env("PWD", docker_dir.to_str().unwrap())
             .env("L2OO_ADDRESS", addresses["L2OutputOracleProxy"].to_string())
             .env(
@@ -320,46 +229,17 @@ impl Stages {
             )
             .current_dir(&docker_dir)
             .output()?;
-        check_command(start_batcher, "Failed to start batcher")?;
-
-        // Step 10.
-        // Start challenger
-
-        // TODO: Deploy the mock dispute game contract
-        let dgf_address = "0x0000000000000000000000000000000000000000";
-
-        println!("Starting challenger...");
-        let start_challenger = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "challenger"])
-            .env("PWD", docker_dir.to_str().unwrap())
-            .env("L2OO_ADDRESS", addresses["L2OutputOracleProxy"].to_string())
-            .env("DGF_ADDRESS", dgf_address)
-            .env(
-                "CHALLENGER_AGENT_CHOICE",
-                self.config.challenger.to_string(),
-            )
-            .current_dir(&docker_dir)
-            .output()?;
-        check_command(start_challenger, "Failed to start challenger")?;
-
-        // Step 11.
-        // Start stateviz
-        let start_stateviz = Command::new("docker-compose")
-            .args(["up", "-d", "--no-deps", "--build", "stateviz"])
-            .env("PWD", docker_dir.to_str().unwrap())
-            .env("L2OO_ADDRESS", addresses["L2OutputOracleProxy"].to_string())
-            .current_dir(&docker_dir)
-            .output()?;
-        check_command(start_stateviz, "Failed to start stateviz")?;
+        check_command(start_rollup, "Failed to start rollup client")?;
+        net::wait_up(OP_NODE_PORT, 30, 1)?;
 
         // Done!
 
-        println!("\n--------------------------");
-        println!("Devnet built successfully!");
-        println!("L1 endpoint: {}", L1_URL);
-        println!("L2 endpoint: {}", L2_URL);
-        println!("Rollup node endpoint: {}", ROLLUP_URL);
-        println!("--------------------------\n");
+        tracing::info!(target: "opup", "--------------------------");
+        tracing::info!(target: "opup", "Devnet built successfully!");
+        tracing::info!(target: "opup", "L1 endpoint: {}", L1_URL);
+        tracing::info!(target: "opup", "L2 endpoint: {}", L2_URL);
+        tracing::info!(target: "opup", "op-node endpoint: {}", OP_NODE_URL);
+        tracing::info!(target: "opup", "--------------------------\n");
 
         Ok(())
     }
